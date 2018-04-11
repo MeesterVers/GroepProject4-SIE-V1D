52afa779ab410dfd69b1d3d0d0634702843a5091
Kadeem Vers <kadeemv@outlook.com>
2018-04-11 14:31:00 +0200

    toont medestudenten status op home

--- src/controller/MedestudentenController.java
+++ src/controller/MedestudentenController.java
@@ -8,116 +8,152 @@ import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import java.util.ArrayList;
 
 import model.PrIS;
+import model.afwezigheid.Afwezigheid;
 import model.klas.Klas;
+import model.les.Les;
 import model.persoon.Student;
+import model.presentie.Presentie;
 import server.Conversation;
 import server.Handler;
 
 public class MedestudentenController implements Handler {
-	private PrIS informatieSysteem;
-	
-	/**
-	 * De StudentController klasse moet alle student-gerelateerde aanvragen
-	 * afhandelen. Methode handle() kijkt welke URI is opgevraagd en laat
-	 * dan de juiste methode het werk doen. Je kunt voor elke nieuwe URI
-	 * een nieuwe methode schrijven.
-	 * 
-	 * @param infoSys - het toegangspunt tot het domeinmodel
-	 */
-	public MedestudentenController(PrIS infoSys) {
-		informatieSysteem = infoSys;
-	}
+    private PrIS informatieSysteem;
+    ArrayList presentie = new ArrayList();
 
-	public void handle(Conversation conversation) {
-		if (conversation.getRequestedURI().startsWith("/student/medestudenten/ophalen")) {
-			ophalen(conversation);
-		} else {
-			opslaan(conversation);
-		}
-	}
-
-	/**
-	 * Deze methode haalt eerst de opgestuurde JSON-data op. Daarna worden
-	 * de benodigde gegevens uit het domeinmodel gehaald. Deze gegevens worden
-	 * dan weer omgezet naar JSON en teruggestuurd naar de Polymer-GUI!
-	 * 
-	 * @param conversation - alle informatie over het request
-	 */
-	private void ophalen(Conversation conversation) {
-		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
-		String lGebruikersnaam = lJsonObjectIn.getString("username");
-		Student lStudentZelf = informatieSysteem.getStudent(lGebruikersnaam);
-		String  lGroepIdZelf = lStudentZelf.getGroepId();
-		
-		Klas lKlas = informatieSysteem.getKlasVanStudent(lStudentZelf);		// klas van de student opzoeken
-
-    List<Student> lStudentenVanKlas = lKlas.getStudenten();	// medestudenten opzoeken
-		
-		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();						// Uiteindelijk gaat er een array...
-		
-		for (Student lMedeStudent : lStudentenVanKlas) {									        // met daarin voor elke medestudent een JSON-object... 
-			if (lMedeStudent == lStudentZelf) 																			// behalve de student zelf...
-				continue;
-			else {
-				String lGroepIdAnder = lMedeStudent.getGroepId();
-				boolean lZelfdeGroep = ((lGroepIdZelf != "") && (lGroepIdAnder==lGroepIdZelf));
-				JsonObjectBuilder lJsonObjectBuilderVoorStudent = Json.createObjectBuilder(); // maak het JsonObject voor een student
-				String lLastName = lMedeStudent.getVolledigeAchternaam();
-				lJsonObjectBuilderVoorStudent
-					.add("id", lMedeStudent.getStudentNummer())																	//vul het JsonObject		     
-					.add("firstName", lMedeStudent.getVoornaam())	
-					.add("lastName", lLastName)				     
-				  .add("sameGroup", lZelfdeGroep);					     
-			  
-			  lJsonArrayBuilder.add(lJsonObjectBuilderVoorStudent);													//voeg het JsonObject aan het array toe				     
-			}
-		}
-    String lJsonOutStr = lJsonArrayBuilder.build().toString();												// maak er een string van
-		conversation.sendJSONMessage(lJsonOutStr);																				// string gaat terug naar de Polymer-GUI!
-	}
-
-	/**
-	 * Deze methode haalt eerst de opgestuurde JSON-data op. Op basis van deze gegevens 
-	 * het domeinmodel gewijzigd. Een eventuele errorcode wordt tenslotte
-	 * weer (als JSON) teruggestuurd naar de Polymer-GUI!
-	 * 
-	 * @param conversation - alle informatie over het request
-	 */
-	private void opslaan(Conversation conversation) {
-		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
-		String lGebruikersnaam = lJsonObjectIn.getString("username");
-		Student lStudent = informatieSysteem.getStudent(lGebruikersnaam);
-		
-		//Het lJsonObjectIn bevat niet enkel strings maar ook een heel (Json) array van Json-objecten.
-		// in dat Json-object zijn telkens het studentnummer en een indicatie of de student 
-		// tot het zelfde team hoort opgenomen.
-		
-		//Het Json-array heeft als naam: "groupMembers"
-  	JsonArray lGroepMembers_jArray = lJsonObjectIn.getJsonArray("groupMembers"); 
-    if (lGroepMembers_jArray != null) { 
-    	// bepaal op basis van de huidige tijd een unieke string
-    	Calendar lCal = Calendar.getInstance();
-    	long lMilliSeconds = lCal.getTimeInMillis();
-    	String lGroepId = String.valueOf(lMilliSeconds);   
-    	
-    	lStudent.setGroepId(lGroepId);
-    	for (int i=0;i<lGroepMembers_jArray.size();i++){
-    		JsonObject lGroepMember_jsonObj = lGroepMembers_jArray.getJsonObject(i );
-    		int lStudentNummer = lGroepMember_jsonObj.getInt("id");
-    		boolean lZelfdeGroep = lGroepMember_jsonObj.getBoolean("sameGroup");
-    		if (lZelfdeGroep) {
-    			Student lGroepStudent = informatieSysteem.getStudent(lStudentNummer);
-     		  lGroepStudent.setGroepId(lGroepId);
-    		}
-    	}
+    public MedestudentenController(PrIS infoSys) {
+        informatieSysteem = infoSys;
     }
-    
-  	JsonObjectBuilder lJob =	Json.createObjectBuilder(); 
-  	lJob.add("errorcode", 0);
-   	//nothing to return use only errorcode to signal: ready!
-  	String lJsonOutStr = lJob.build().toString();
- 		conversation.sendJSONMessage(lJsonOutStr);					// terug naar de Polymer-GUI!
-	}
+
+    public void handle(Conversation conversation) {
+        if (conversation.getRequestedURI().startsWith("/student/medestudenten/ophalen")) {
+            ophalen(conversation);
+        } else {
+            opslaan(conversation);
+        }
+    }
+
+    /**
+     * Deze methode haalt eerst de opgestuurde JSON-data op. Daarna worden
+     * de benodigde gegevens uit het domeinmodel gehaald. Deze gegevens worden
+     * dan weer omgezet naar JSON en teruggestuurd naar de Polymer-GUI!
+     *
+     * @param conversation - alle informatie over het request
+     */
+    private void ophalen(Conversation conversation) {
+        JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
+        ArrayList<Les> rooster = informatieSysteem.getRooster();
+        String les = lJsonObjectIn.getString("les");
+        String student = lJsonObjectIn.getString("username");
+        String[] parts = les.split("\\.");
+        String vakCode = parts[0];
+        String datum = parts[1];
+        String startTijd = parts[2];
+        JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
+
+        for (Les l : rooster) {
+            if (l.getDatum().equals(datum) && l.getVakCode().equals(vakCode) && l.getStartTijd().equals(startTijd)) {
+                String klascode = l.getKlasCode();
+                Klas klasobj = informatieSysteem.getKlasviacode(klascode);
+                JsonObjectBuilder lJsonObjectBuilderLes = Json.createObjectBuilder();
+                lJsonObjectBuilderLes
+                        .add("datum", l.getDatum())
+                        .add("klasCode", l.getKlasCode())
+                        .add("startTijd", l.getStartTijd())
+                        .add("eindTijd", l.getEindTijd())
+                        .add("vakCode", l.getVakCode());
+                JsonArrayBuilder lJsonArrayBuilder1 = Json.createArrayBuilder();
+                for (Student std : klasobj.getStudenten()) {
+                    JsonObjectBuilder lJsonObjectBuilderStudent = Json.createObjectBuilder();
+                    lJsonObjectBuilderStudent
+                            .add("id", std.getStudentNummer())
+                            .add("naam", std.getVoornaam())
+                            .add("achternaam", std.getVolledigeAchternaam())
+                            .add("soort", "aanwezig");
+                    for (Afwezigheid afw : l.getAfwezigheden()) {
+                        if (afw.getStudent().getGebruikersnaam().equals(std.getGebruikersnaam())) {
+                            lJsonObjectBuilderStudent
+                            .add("soort", afw.getSoort())
+                            .add("startDatum", afw.getStartDatum())
+                            .add("eindDatum", afw.getEindDatum())
+                            .add("beschrijving", afw.getBeschrijving());
+                        }
+                    }
+
+                    for (Afwezigheid afw : informatieSysteem.getPeriodeAfwezigheden()) {
+                        if (afw.getStudent().getGebruikersnaam().equals(std.getGebruikersnaam())) {
+                            lJsonObjectBuilderStudent
+                                    .add("soort", afw.getSoort())
+                                    .add("startDatum", afw.getStartDatum())
+                                    .add("eindDatum", afw.getEindDatum())
+                                    .add("beschrijving", afw.getBeschrijving());
+                        }
+                    }
+                    lJsonArrayBuilder1.add(lJsonObjectBuilderStudent);
+                }
+                lJsonObjectBuilderLes.add("leerlingen", lJsonArrayBuilder1);
+                lJsonArrayBuilder.add(lJsonObjectBuilderLes);
+            }
+        }
+
+        String lJsonOutStr = lJsonArrayBuilder.build().toString();
+        conversation.sendJSONMessage(lJsonOutStr);
+
+    }
+
+
+    /**
+     * Deze methode haalt eerst de opgestuurde JSON-data op. Op basis van deze gegevens
+     * het domeinmodel gewijzigd. Een eventuele errorcode wordt tenslotte
+     * weer (als JSON) teruggestuurd naar de Polymer-GUI!
+     *
+     * @param conversation - alle informatie over het request
+     */
+    private void opslaan(Conversation conversation) {
+        JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
+        String vakCode= lJsonObjectIn.getString("vakCode");
+        String klasCode = lJsonObjectIn.getString("klasCode");
+        String datum = lJsonObjectIn.getString("datum");
+
+        Les rooster_les = null;
+        for (Les l : informatieSysteem.getRooster()) {
+            if (l.getVakCode().equals(vakCode) && l.getKlasCode().equals(klasCode) && l.getDatum().equals(datum)) {
+                rooster_les = l;
+            }
+        }
+
+        //Het lJsonObjectIn bevat niet enkel strings maar ook een heel (Json) array van Json-objecten.
+        // in dat Json-object zijn telkens het studentnummer en een indicatie of de student
+        // tot het zelfde team hoort opgenomen.
+
+        //Het Json-array heeft als naam: "groupMembers"
+        JsonArray leerlingen = lJsonObjectIn.getJsonArray("leerlingen");
+
+        rooster_les.leegPresenties();
+        if (leerlingen != null) {
+            // bepaal op basis van de huidige tijd een unieke string
+            for (int i=0;i<leerlingen.size();i++){
+                JsonObject leerling = leerlingen.getJsonObject(i);
+                String status = leerling.getString("status");
+                String id = leerling.getString("id");
+                Student std = informatieSysteem.getStudent(Integer.parseInt(id));
+                rooster_les.voegPresentieToe(new Presentie(std, status));
+            }
+        }
+
+//        for (Les l : informatieSysteem.getRooster()) {
+//            for (Presentie p : l.getPresenties()) {
+//                System.out.println(p);
+//            }
+//        }
+//        System.out.println("\n");
+
+        JsonObjectBuilder lJob = Json.createObjectBuilder();
+        lJob.add("errorcode", 0);
+        //nothing to return use only errorcode to signal: ready!
+        String lJsonOutStr = lJob.build().toString();
+        conversation.sendJSONMessage(lJsonOutStr);					// terug naar de Polymer-GUI!
+    }
 }
